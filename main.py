aa269d4d8911cc27ad8f38d4526288a65ea357d5
Dhruvin111 <108988297+Dhruvin111@users.noreply.github.com>
2024-09-16 21:23:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,30 +123,6 @@ print("Accuracy is:", classify.accuracy(classifier, test_data))
 
 print(classifier.show_most_informative_features(5))
 
-
-def remove_noise(tweet_tokens, stop_words=()):
-    cleaned_tokens = []
-
-    for token, tag in pos_tag(tweet_tokens):
-        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|' \
-                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
-        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
-
-        if tag.startswith("NN"):
-            pos = 'n'
-        elif tag.startswith('VB'):
-            pos = 'v'
-        else:
-            pos = 'a'
-
-        lemmatizer = WordNetLemmatizer()
-        token = lemmatizer.lemmatize(token, pos)
-
-        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
-            cleaned_tokens.append(token.lower())
-    return cleaned_tokens
-
-
 def display_accuracy():
     accuracy = classify.accuracy(classifier, test_data)
     accuracy_text.config(state=tk.NORMAL)
