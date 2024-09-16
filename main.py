b1d7e1b539c5b0c4ef55315143e2dc42328ed8f3
Dhruvin111 <108988297+Dhruvin111@users.noreply.github.com>
2024-09-16 21:24:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
 tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-
-
 def lemmatize_sentence(tokens):
     lemmatizer = WordNetLemmatizer()
     lemmatized_sentence = []
